b13eb2a348cc377e2338e293fee1dd5c73cebc1a
Alexander Sinigerov <sinigers@yahoo.com>
2022-07-08 10:16:24 +0300

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+import Excercises from "../components/Excercises";
+import SearchExcercises from "../components/SearchExcercises";
+import HeroBanner from "../components/HeroBanner";
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExcercises />
+      <Excercises />
+    </Box>
+  );
+};
+
+export default Home;
